34412412209d37b5286f77079685c7957311da9d
sirius <736784326zcy@gmail.com>
2020-06-10 03:08:54 +0200

    new try

--- hanabi_learning_environment/agents/chenyang_agent.py
+++ hanabi_learning_environment/agents/chenyang_agent.py
@@ -15,6 +15,7 @@
 
 from hanabi_learning_environment.rl_env import Agent
 from hanabi_learning_environment.pyhanabi import HanabiCard
+from hanabi_learning_environment.pyhanabi import HanabiCardKnowledge
 
 
 class ChenyangAgent(Agent):
@@ -29,31 +30,42 @@ class ChenyangAgent(Agent):
         self.max_information_tokens = config.get('information_tokens', 8)
 
     @staticmethod
-    def playable_card(card_knowledge, fireworks):
-        """A card is playable if it can be placed on the fireworks pile."""
-        possible_card = ChenyangAgent.get_possible(card_knowledge)
+    def playable_card(card_index, observation, fireworks):
+        possible_card = ChenyangAgent.get_possible(card_index, observation)
         all_playable_card = ChenyangAgent.all_playable_cards(fireworks)
         return set(possible_card).issubset(set(all_playable_card))
 
-    def get_possible(self, card_knowledge):
+    @staticmethod
+    def discardble_card(card_index, observation, fireworks):
+        possible_card = ChenyangAgent.get_possible(card_index, observation)
+        all_discardble_card = ChenyangAgent.all_discardble_cards(fireworks)
+        return set(possible_card).issubset(set(all_discardble_card))
+
+    @staticmethod
+    def get_possible(card_index, observation):
+        card = observation['observed_hands'][0][card_index]
         possible_colors = []
         possible_ranks = []
-        for color in ChenyangAgent.colors:
-            if card_knowledge.color_plausible(color):
-                possible_colors.append(color)
-        for rank in ChenyangAgent.ranks:
-            if card_knowledge.rank_plausible(rank):
-                possible_ranks.append(rank)
+        if card['color'] is None:
+            [possible_colors.append(color) for color in ChenyangAgent.colors]
+        else:
+            possible_colors.append(card['color'])
+        if card['rank'] is None:
+            [possible_ranks.append(rank) for rank in ChenyangAgent.ranks]
+        else:
+            possible_ranks.append(card['rank'])
         possible_cards = [HanabiCard(color, rank) for color in possible_colors for rank in possible_ranks]
         return possible_cards
 
-    def all_playable_cards(self, fireworks):
+    @staticmethod
+    def all_playable_cards(fireworks):
         all_playable_cards = []
         for card in fireworks:
             all_playable_cards.append(card)
         return all_playable_cards
 
-    def all_discardble_cards(self, fireworks):
+    @staticmethod
+    def all_discardble_cards(fireworks):
         all_discardble_cards = []
         for card in fireworks:
             [all_discardble_cards.append(HanabiCard(card['color'], rank)) for rank in range(card['rank'])]
@@ -61,33 +73,35 @@ class ChenyangAgent(Agent):
 
     def act(self, observation):
         """Act based on an observation."""
-
         if observation['current_player_offset'] != 0:
             return None
 
-        # Check if there are any pending hints and play the card corresponding to
-        # the hint.
-        for card_index, hint in enumerate(observation['card_knowledge'][0]):
-            if hint['color'] is not None:
-                if observation['life_tokens'] > 1:
-                    return {'action_type': 'PLAY', 'card_index': card_index}
+        fireworks = observation['fireworks']
+        my_hand = observation['observed_hands'][0]
+        you_hand = observation['observed_hands'][1]
+
+        for card_index, card in enumerate(my_hand):
+            if ChenyangAgent.playable_card(card_index, observation, fireworks):
+                return {'action_type': 'PLAY', 'card_index': card_index}
+
+        if observation['information tokens'] < self.max_information_tokens:
+            for card_index, card in enumerate(my_hand):
+                if ChenyangAgent.discardble_card(card_index, observation, fireworks):
+                    return {'action_type': 'DISCARD', 'card_index': card_index}
+
+        you_hints = observation['card_knowledge'][1]
 
         # Check if it's possible to hint a card to your colleagues.
-        fireworks = observation['fireworks']
+
         if observation['information_tokens'] > 0:
             # Check if there are any playable cards in the hands of the colleagues.
-            for player_offset in range(1, observation['num_players']):
-                player_hand = observation['observed_hands'][player_offset]
-                player_hints = observation['card_knowledge'][player_offset]
-                # Check if the card in the hand of the colleagues is playable.
-                for card, hint in zip(player_hand, player_hints):
-                    if ChenyangAgent.playable_card(card,
-                                                   fireworks) and hint['color'] is None:
-                        return {
-                            'action_type': 'REVEAL_COLOR',
-                            'color': card['color'],
-                            'target_offset': player_offset
-                        }
+            for card_index, card in enumerate(you_hand):
+                if ChenyangAgent.playable_card(card_index, observation, fireworks) and card['rank'] is None:
+                    return {
+                        'action_type': 'REVEAL_RANK',
+                        'rank': card['rank'],
+                        'target_offset': 1
+                    }
 
         # If no card is hintable then discard or play.
         if observation['information_tokens'] < self.max_information_tokens:
--- hanabi_learning_environment/agents/simple_agent.py
+++ hanabi_learning_environment/agents/simple_agent.py
@@ -11,66 +11,66 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Chenyang Agent."""
+"""Simple Agent."""
 
 from hanabi_learning_environment.rl_env import Agent
 
 
-class ChenyangAgent(Agent):
-  """Agent that applies a simple heuristic."""
+class SimpleAgent(Agent):
+    """Agent that applies a simple heuristic."""
 
-  def __init__(self, config, *args, **kwargs):
-    """Initialize the agent."""
-    self.config = config
-    # Extract max info tokens or set default to 8.
-    self.max_information_tokens = config.get('information_tokens', 8)
+    def __init__(self, config, *args, **kwargs):
+        """Initialize the agent."""
+        self.config = config
+        # Extract max info tokens or set default to 8.
+        self.max_information_tokens = config.get('information_tokens', 8)
 
-  @staticmethod
-  def playable_card(card, fireworks):
-    """A card is playable if it can be placed on the fireworks pile."""
-    return card['rank'] == fireworks[card['color']]
+    @staticmethod
+    def playable_card(card, fireworks):
+        """A card is playable if it can be placed on the fireworks pile."""
+        return card['rank'] == fireworks[card['color']]
 
-  def act(self, observation):
-    """Act based on an observation."""
-    # colors={'G','W','Y','B','R'}
-    # ranks={1,2,3,4,5}
-    
-    if observation['current_player_offset'] != 0:
-      return None
+    def act(self, observation):
+        """Act based on an observation."""
+        # colors={'G','W','Y','B','R'}
+        # ranks={1,2,3,4,5}
 
-    # Check if there are any pending hints and play the card corresponding to
-    # the hint.
-    for card_index, hint in enumerate(observation['card_knowledge'][0]):
-      if hint['color'] is not None:
-        if observation['life_tokens']>1:
-          return {'action_type': 'PLAY', 'card_index': card_index}
-        
-    # Check if it's possible to hint a card to your colleagues.
-    fireworks = observation['fireworks']
-    if observation['information_tokens'] > 0:
-      # Check if there are any playable cards in the hands of the colleagues.
-      for player_offset in range(1, observation['num_players']):
-        player_hand = observation['observed_hands'][player_offset]
-        player_hints = observation['card_knowledge'][player_offset]
-        # Check if the card in the hand of the colleagues is playable.
-        for card, hint in zip(player_hand, player_hints):
-          if ChenyangAgent.playable_card(card,
-                                       fireworks) and hint['color'] is None:
-            return {
-                'action_type': 'REVEAL_COLOR',
-                'color': card['color'],
-                'target_offset': player_offset
-            }
+        if observation['current_player_offset'] != 0:
+            return None
 
-    # If no card is hintable then discard or play.
-    if observation['information_tokens'] < self.max_information_tokens:
-      for card_index, hint in enumerate(observation['card_knowledge'][0]):
-       if hint['color'] is None and hint['rank'] is None:
-         return {'action_type': 'DISCARD', 'card_index': card_index}
-    else:
-      for card_index, hint in enumerate(observation['card_knowledge'][1]):
-        if hint['rank'] is None:
-            return{ 'action_type': 'REVEAL_RANK',
-                    'rank' : observation['observed_hands'][1][card_index]['rank'],
-                    'target_offset': 1}
-    print('################################stop here#########################################')
+        # Check if there are any pending hints and play the card corresponding to
+        # the hint.
+        for card_index, hint in enumerate(observation['card_knowledge'][0]):
+            if hint['color'] is not None:
+                if observation['life_tokens'] > 1:
+                    return {'action_type': 'PLAY', 'card_index': card_index}
+
+        # Check if it's possible to hint a card to your colleagues.
+        fireworks = observation['fireworks']
+        if observation['information_tokens'] > 0:
+            # Check if there are any playable cards in the hands of the colleagues.
+            for player_offset in range(1, observation['num_players']):
+                player_hand = observation['observed_hands'][player_offset]
+                player_hints = observation['card_knowledge'][player_offset]
+                # Check if the card in the hand of the colleagues is playable.
+                for card, hint in zip(player_hand, player_hints):
+                    if SimpleAgent.playable_card(card,
+                                                 fireworks) and hint['color'] is None:
+                        return {
+                            'action_type': 'REVEAL_COLOR',
+                            'color': card['color'],
+                            'target_offset': player_offset
+                        }
+
+        # If no card is hintable then discard or play.
+        if observation['information_tokens'] < self.max_information_tokens:
+            for card_index, hint in enumerate(observation['card_knowledge'][0]):
+                if hint['color'] is None and hint['rank'] is None:
+                    return {'action_type': 'DISCARD', 'card_index': card_index}
+        else:
+            for card_index, hint in enumerate(observation['card_knowledge'][1]):
+                if hint['rank'] is None:
+                    return {'action_type': 'REVEAL_RANK',
+                            'rank': observation['observed_hands'][1][card_index]['rank'],
+                            'target_offset': 1}
+        print('################################stop here#########################################')
