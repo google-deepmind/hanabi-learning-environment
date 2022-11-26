d27a107cf35a3715caff75756721a793350d0be9
Alexander Bleissem <alexander@bleissem.com>
2022-11-26 22:34:55 +0100

    add long description

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from skbuild import setup
 
 setup(
     name='ws2223-group7-hanabi-learning-environment',
-    version='0.0.1',
-    description='Learning environment for the game of hanabi.',
+    version='0.0.1',    
+    description='ws2223-group7 Learning environment for the game of hanabi.',
+    long_description='ws2223-group7 Learning environment for the game of hanabi.',
     author='ws2223-group7/hanabi-learning-environment',
     packages=['hanabi_learning_environment', 'hanabi_learning_environment.agents'],
     install_requires=['cffi']
