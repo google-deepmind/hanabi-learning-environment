37ef36d27b4cbe6ea8c14db950ab7e528859b948
Alexander Bleissem <alexander@bleissem.com>
2022-11-29 00:22:58 +0100

    modify setup.cfg, setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@ from skbuild import setup
 
 setup(
     name='ws2223-group7-hanabi-learning-environment',
-    version='0.0.1',    
+    version='0.0.2',    
     description='ws2223-group7 Learning environment for the game of hanabi.',
     long_description='ws2223-group7 Learning environment for the game of hanabi.',
+    long_description_content_type="text/markdown",
     author='ws2223-group7/hanabi-learning-environment',
     packages=['hanabi_learning_environment', 'hanabi_learning_environment.agents'],
-    install_requires=['cffi']
+    install_requires=['cffi'],
+    python_requires=">=3.6",
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "Operating System :: POSIX :: Linux",
+    ]    
 )
