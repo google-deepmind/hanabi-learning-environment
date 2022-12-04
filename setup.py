86a4309acadfbd786f6f85dd5790b6cae3c6f12a
Alexander Bleissem <alexander@bleissem.com>
2022-12-04 22:40:18 +0100

    remove setup.cfg, set version to 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name='ws2223-group7-hanabi-learning-environment',
-    version='0.0.3',    
+    version='0.0.4',    
     description='ws2223-group7 Learning environment for the game of hanabi.',
     long_description='ws2223-group7 Learning environment for the game of hanabi.',
     long_description_content_type="text/markdown",
