80551a882828aeeef2a6870fae5dedb0768baf8e
Alexander Bleissem <alexander@bleissem.com>
2022-12-13 21:41:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from skbuild import setup
 
 setup(
-    name='ws2223-group7-hanabi-learning-environment',
-    version='0.0.4',    
-    description='ws2223-group7 Learning environment for the game of hanabi.',
-    long_description='ws2223-group7 Learning environment for the game of hanabi.',
+    name='ws2223-group7-hanabi-learning-environment_bad',
+    version='0.0.1',    
+    description='ws2223-group7 Learning environment for the game of hanabi bad agent.',
+    long_description='ws2223-group7 Learning environment for the game of hanabi bad agent.',
     long_description_content_type="text/markdown",
-    author='ws2223-group7/hanabi-learning-environment',
+    author='ws2223-group7/hanabi-learning-environment_bad',
     packages=['hanabi_learning_environment', 'hanabi_learning_environment.agents'],
     install_requires=['cffi'],
     python_requires=">=3.6",
