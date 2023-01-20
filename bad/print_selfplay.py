d161bc34fc07d81c270b4cbb8927708871d44261
Alexander Bleissem <alexander@bleissem.com>
2023-01-20 14:39:42 +0100

    prints rewards

--- bad/print_selfplay.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=missing-module-docstring, wrong-import-position, import-error, no-member, no-name-in-module, too-many-function-args, unnecessary-pass, too-few-public-methods
-from hanabi_learning_environment.rl_env import HanabiEnv
-
-
-class PrintSelfPlay:
-    '''print self play'''
-    def print(self, hanabi_environment: HanabiEnv) -> None:
-        '''print'''
-        pass
