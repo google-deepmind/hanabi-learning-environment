35517cd1365d813c777a63585dfcd1df03bdfa44
Alexander Bleissem <alexander@bleissem.com>
2023-01-20 14:46:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from skbuild import setup
 
 setup(
     name='ws2223-group7-hanabi-learning-environment_bad',
-    version='0.0.2',
+    version='0.0.3',
     description='ws2223-group7 Learning environment for the game of hanabi bad agent.',
     long_description='ws2223-group7 Learning environment for the game of hanabi bad agent.',
     long_description_content_type="text/markdown",
